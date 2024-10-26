4bb2a8634b461315f79622a33ba9dbdbd4412c44
Pankaj Singh <psrathore13101@gmail.com>
2024-10-26 09:02:15 +0530

    accept and reject feature for assignment

--- frontend/src/pages/SubmittedAssignments.jsx
+++ frontend/src/pages/SubmittedAssignments.jsx
@@ -22,7 +22,36 @@ const SubmittedAssignments = () => {
 
     useEffect(() => {
         getSubmittedAssignments();
-    }, [])
+    }, []);
+
+    const handleReject = async () => {
+        const response = await fetch(`http://localhost:3000/admin/assignments/6719bd28e97ff61c55745cca/reject`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(),
+            credentials: "include"
+        })
+
+        const data = await response.json();
+        console.log(data);
+
+    }
+
+    const handleAccept = async () => {
+        const response = await fetch(`http://localhost:3000/admin/assignments/6719bd28e97ff61c55745cca/accept`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(),
+            credentials: "include"
+        })
+
+        const data = await response.json();
+        console.log(data);
+    }
 
   return (
     <div>
@@ -32,8 +61,8 @@ const SubmittedAssignments = () => {
             
             <div key={assignment._id} className='w-1/4 mt-4'>
                 <p>{assignment.task}</p>
-                <button>Reject</button>
-                <button className='ml-6'>Accept</button>
+                <button onClick={handleReject}>Reject</button>
+                <button onClick={handleAccept} className='ml-6'>Accept</button>
             </div>
         )
 
